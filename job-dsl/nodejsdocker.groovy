23952a62d5298e637aa9721275b651c1d2d842b6
antonmaskalik <90907960+antonmaskalik@users.noreply.github.com>
2022-07-11 16:01:01 +0300

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-         git('https://github.com/antonmaskalik/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+         git('git://github.com/antonmaskalik/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('antonmaskalik')
             node / gitConfigEmail('anton.maskalik@gmail.com')
         }
