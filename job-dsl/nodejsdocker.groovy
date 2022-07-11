91dfe4d439c52b6a475ceeb1bc78d6d458254375
antonmaskalik <90907960+antonmaskalik@users.noreply.github.com>
2022-07-11 14:52:47 +0300

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -15,7 +15,7 @@ job('NodeJS Docker example') {
     steps {
         dockerBuildAndPublish {
             repositoryName('antonmaskalik/nodejs')
-            tag('${GIT_COMMIT}')
+            tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
